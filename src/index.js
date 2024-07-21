75467d85bd269bb43edf31101410894753f4554b
Dave J <29786887+daveswork@users.noreply.github.com>
2024-07-21 16:33:12 -0400

    All local tests pass.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 const init = () => {
+    const movieForm = document.querySelector("form");
+    movieForm.addEventListener("submit", (event)=> {
+        event.preventDefault();
+        const movieInput = document.querySelector("input#searchByID");
+        // console.log(movieInput.value);
+
+        fetch(`http://localhost:3000/movies/${movieInput.value}`)
+            .then((response) => response.json())
+            .then((data) => {
+                const title = document.querySelector("section#movieDetails h4");
+                const summary = document.querySelector("section#movieDetails p");
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+            })
+
+    })
+    
   
 }
 
